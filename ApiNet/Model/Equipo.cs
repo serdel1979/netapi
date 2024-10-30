08c86b937cfd7767504e233df5b6f12b548b7b7e
Sergio De Luca <sdlbsso@gmail.com>
2024-10-30 15:22:00 -0300

    agregado migraciones

--- ApiNet/Model/Equipo.cs
+++ ApiNet/Model/Equipo.cs
@@ -1,8 +1,13 @@
-﻿namespace ApiNet.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiNet.Model
 {
     public class Equipo
     {
+        public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public string Descripción { get; set; }
     }
 }
